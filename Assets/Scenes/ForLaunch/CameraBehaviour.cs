cb9fe35192bc568ebf27e3539d42dd6c552f581e
Gundars Pelns <Gundars2.Pelns@live.uwe.ac.uk>
2018-06-15 17:31:32 +0100

    Testing things out in 'Launch'
    Just made a scene to work on the movement

---
+++ Assets/Scenes/ForLaunch/CameraBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    private float startSize = 1.0f;
+    private float endSize = 5.0f;
+
+    static float t = 0.0f;
+
+    private Camera m_cam;
+    private bool launched;
+
+	// Use this for initialization
+	void Start () {
+        m_cam = GetComponent<Camera>();
+        launched = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Fire1") > 0f)
+        {
+            launched = true;
+        }
+
+        if (launched)
+        {
+            Begin();
+        }
+	}
+
+    void Begin()
+    {
+        t += 0.5f * Time.deltaTime;
+        m_cam.orthographicSize = Mathf.Lerp(startSize, endSize, t);
+    }
+
+}
